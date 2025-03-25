640b6530f72ff65accb28b09475fabfd15e24493
zhanghaoxvan <3592114583@qq.com>
2025-03-25 21:16:42 +0800

    Do some update...

--- include/install.hpp
+++ include/install.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include "includes.hpp"
+#include "progressbar.hpp"
 
 namespace cppkg::install {
     void install(const std::string &package_name) {
-
+        std::cout << "Install Process:";
+        progressbar pb(100, true, std::cout);
+        // system("wget ")
     }
 }
