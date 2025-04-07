06abe237dca38d4f29a82c9f486eb28d080d13c6
zhanghaoxvan <3592114583@qq.com>
2025-04-07 13:34:51 +0800

    Comple...

--- include/help.hpp
+++ include/help.hpp
@@ -2,7 +2,15 @@
 #include "includes.hpp"
 
 namespace cppkg::help {
+    void show_icon() {
+        std::cout << "   ___   _ __    _ __   | | __   __ _ " << std::endl;
+        std::cout << "  / __| | '_ \\  | '_ \\  | |/ /  / _` |" << std::endl;
+        std::cout << " | (__  | |_) | | |_) | |   <  | (_| |" << std::endl;
+        std::cout << "  \\___| | .__/  | .__/  |_|\\_\\  \\__, |" << std::endl;
+        std::cout << "        |_|     |_|             |___/ " << std::endl;
+    }
     void help() {
+        show_icon();
         std::cout << "Usage:" << std::endl;
         std::cout << "cppkg install <packages-file>:" << std::endl;
         std::cout << "    cppkg install <package> - install a package." << std::endl;
@@ -11,6 +19,6 @@ namespace cppkg::help {
         std::cout << "cppkg list             - list all installed packages." << std::endl;
         std::cout << "cppkg search <package> - search a package." << std::endl;
         std::cout << "cppkg help             - show this text and exit." << std::endl;
-        std::cout << "cppkg version          - show version information." << std::endl;
+        std::cout << "cppkg version          - show version of cppkg." << std::endl;
     }
 }
