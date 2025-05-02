9be79808bd50ea84766f8d0e59ebc2b915cfdcf6
zhanghaoxvan <3592114583@qq.com>
2025-05-02 10:19:25 +0800

    Change Install

--- src/install.cpp
+++ src/install.cpp
@@ -1,14 +1,17 @@
 #include "../include/install.hpp"
 
 void cppkg::install::install_local(const std::string &package_name) {
+    std::string package = std::string(package_name.begin(), package_name.begin() + package_name.rfind("."));
     // Rename File *.cppkg to *.zip
-    system(("mv " + package_name + " " + std::string(package_name.begin(), package_name.begin() + package_name.rfind(".")) + ".zip").c_str());
+    system(("mv " + package_name + " " + package + ".zip").c_str());
     // unzip file
-    system((std::string(package_name.begin(), package_name.begin() + package_name.rfind(".")) + ".zip").c_str());
+    system(("unzip " + package + ".zip").c_str());
     // copy the floder to ~/.cppkg
-    system(("cp -r " + std::string(package_name.begin(), package_name.begin() + package_name.rfind(".")) + " ~/.cppkg/").c_str());
+    system(("cp -r " + package + " ~/.cppkg/").c_str());
     // delete the zip file
     system(("rm " + std::string(package_name.begin(), package_name.begin() + package_name.rfind(".")) + ".zip").c_str());
+    // Create a symlink to the package at /usr/include
+    
 }
 
 void cppkg::install::install_web(const std::string &package_name) {
